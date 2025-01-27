511a5bb21b6853ebbb5f535cc417c133e649c8a4
JoaquinFigueroa5 <hfigueroa-2023417@kinal.edu.gt>
2025-01-27 11:58:18 -0600

    Creacion del servidor

---
+++ configs/mongo.js
@@ -0,0 +1,30 @@
+'use stritc';
+import mongoose from "mongoose";
+
+export const dbConnection = async () => {
+
+    try{
+        mongoose.connection.on('error',()=>{
+            console.log('MongoDB | coul not be connected to MONGOdB')
+            mongoose.disconnect();
+        });
+        mongoose.connection.on('connecting', ()=>{
+            console.log('MongoDB | connected to MongoDB')
+        })
+        mongoose.connection.on('open', ()=>{
+            console.log('MongoDB | connected to database');
+        });
+        mongoose.connection.on('reconnected', ()=>{
+            console.log('MongoDB | reconnected to MongoDB');
+        });
+        mongoose.connection.on('disconnected',()=>{
+            console.log('MongoDB | disconnected')
+        })
+        mongoose.connect(process.env.URI_MONGO, {
+            serverSelectionTimeoutMS : 5000,
+            maxPoolSize : 50,
+        });
+    }catch(error){
+        console.log('Database connection failed', error)
+    }
+}
